3d9205fa0f074a5470f85517b113078eb6db6f9a
m4x1mu512 <trickster1212@xmail.ru>
2026-09-19 14:35:58 +0400

    Create Color.kt

---
+++ app/src/main/java/com/example/temp/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.temp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBg = Color(0xFFF3FBF5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightPrimary = Color(0xFF14B8A6)      // бирюза
+val LightSecondary = Color(0xFF38BDF8)    // голубой
+val LightTertiary = Color(0xFF8B5CF6)     // фиолетовый
+
+val DarkBg = Color(0xFF0E1512)
+val DarkSurface = Color(0xFF15201B)
+val DarkPrimary = Color(0xFF2DD4BF)
+val DarkSecondary = Color(0xFF60A5FA)
+val DarkTertiary = Color(0xFFA78BFA)
